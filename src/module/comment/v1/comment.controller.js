363f5ba6e17aaa8c78b4b3a2f59e8fcbec53eb20
Mehdi <mahdizareiofficial@gmail.com>
2025-07-23 21:09:46 +0330

    coding comments logic

---
+++ src/module/comment/v1/comment.controller.js
@@ -0,0 +1,107 @@
+const { errorResponse, successResponse } = require("../../../helper/responseMessage");
+const Comment = require("../../../model/Comment");
+const Article = require("../../../model/Article");
+const { isValidObjectId } = require("mongoose");
+
+exports.getAll = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.create = async (req, res, next) => {
+  try {
+    const { articleId, parentId, content, score } = req.body;
+    const userId = req.user._id;
+
+    const isArticleExist = await Article.findById(articleId);
+    if (!isArticleExist) {
+      return errorResponse(res, 404, "مقاله ای با این شناسه یافت نشد.");
+    }
+
+    if (parentId) {
+      const mainComment = await Comment.findOne({ _id: parentId, status: "APPROVED" });
+      if (!mainComment) {
+        return errorResponse(res, 404, "کامنت اصلی با این شناسه یافت نشد.");
+      }
+    }
+
+    const isRepetitive = await Comment.findOne({ content, userId });
+    if (isRepetitive) {
+      return errorResponse(res, 409, "این نظر قبلا توسط شما ثبت شده است.");
+    }
+
+    await Comment.create({
+      articleId,
+      content,
+      parentId,
+      score,
+      userId,
+      status: "PENDING",
+    });
+
+    return successResponse(res, 201, "نظر شما با موفقیت ثبت و پس از بازبینی توسط مدیر نمایش داده میشود.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getOne = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.articleComments = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.changeStatus = async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    const { status } = req.body;
+
+    if (!isValidObjectId(commentId)) {
+      return errorResponse(res, 409, "شناسه کامنت معتبر نمی باشد.");
+    }
+
+    const isCommentExist = await Comment.findByIdAndUpdate(commentId, { status });
+    if (!isCommentExist) {
+      return errorResponse(res, 404, "کامنتی با این شناسه یافت نشد.");
+    }
+
+    return successResponse(res, 200, "عملیات با موفقیت انجام شد.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+    const { commentId } = req.params;
+    if (!isValidObjectId(commentId)) {
+      return errorResponse(res, 409, "شناسه کامنت معتبر نمی باشد.");
+    }
+
+    const remove = await Comment.findByIdAndDelete(commentId);
+    if (!remove) {
+      return errorResponse(res, 404, "کامنتی با این شناسه یافت نشد.");
+    }
+
+    return successResponse(res, 200, "کامنت مورد نظر با موفقیت حذف گردید.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.toggleLikeComment = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
